430bbcd30937c37f611766b56d4240e0e1d1c106
Colin Hill <cwh@colinwhill.com>
2018-05-14 20:08:35 +0200

    [add] Sony User Activity Flow

--- src/main/java/com/mediaroids/ContentProviders/Sony/SonyRouteBuilder.java
+++ src/main/java/com/mediaroids/ContentProviders/Sony/SonyRouteBuilder.java
@@ -2,7 +2,8 @@ package com.mediaroids.ContentProviders.Sony;
 
 import SonyClient.proxy.SonyService;
 import SonyClient.proxy.SonyServiceService;
-import com.mediaroids.Movies.MovieAggregationStrategy;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 /**
@@ -14,28 +15,29 @@ public class SonyRouteBuilder extends RouteBuilder {
      * Let's configure the Camel routing rules using Java code...
      */
 
-
     public void configure() {
 
         SonyService service = new SonyServiceService().getSonyServicePort();
-        System.out.println(service.sendMovies("MediaRoids"));
 
         restConfiguration()
-                .host("localhost")
-                .port(8888);
+            .host("localhost")
+            .port(8888);
 
         rest("/contentProviders")
-                .produces("application/json")
-
-                .get().to("direct:getFromSony")
-
-                .get("/{id}").to("direct:getFromSony");
+            .produces("application/json")
+            .get().to("direct:getFromSony")
+            .get("/{id}").to("direct:getFromSony");
 
 
-        from("direct:getFromSony")
-                .setBody(simple(service.sendMovies("Mediaroids").toString()));
-
-//simple("{\"recommendation\":\"You should also watch my movie.\"}")
+        from("queue:sonyActivity")
+            .process(new Processor() {
+                @Override
+                public void process(Exchange exchange) throws Exception {
+                    // TODO: Wait till we have defined data.
+                }
+            })
+                .to("stream:out");
+//            .to(service.sendUserActivity(body()));
 
     }
 }
--- src/main/java/com/mediaroids/UserActivity/UserActivityRouteBuilder.java
+++ src/main/java/com/mediaroids/UserActivity/UserActivityRouteBuilder.java
@@ -1,5 +1,9 @@
 package com.mediaroids.UserActivity;
 
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.PollingConsumer;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.JndiRegistry;
 
@@ -15,25 +19,50 @@ public class UserActivityRouteBuilder extends RouteBuilder {
 
     public void configure() {
 
+
+
         restConfiguration()
                 .host("localhost")
                 .port(8888);
 
         rest("/userActivities")
-                .produces("application/json")
-
-                .get().to("direct:getUserActivities")
-
-                .get("/{id}").to("direct:getUserActivity");
+            .produces("application/json")
+            .get().to("direct:getUserActivities")
+            // ?played=true&userId={userId}&contentType={contentType}&contentId={contentId} - example params
+            .post().to("direct:createUserActivityData")
+            .get("/{id}").to("direct:getUserActivity");
 
 
         from("direct:getUserActivities")
                 .to("http://localhost:13761/api/userActivities/?bridgeEndpoint=true");
 
-        
 
-        from("direct:getUserActivityData")
-                .setBody(simple("{\"title\":\"The Big Bang Theory\"}"));
+        from("direct:createUserActivityData")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        Endpoint endpoint = getContext().getEndpoint("direct:createUserActivity");
+                        PollingConsumer consumer = endpoint.createPollingConsumer();
+
+
+                        consumer.start();
+
+                        Exchange result = consumer.receive();
+
+                        exchange.getIn().setBody(result.getIn().getBody());
+
+                        consumer.stop();
+                    }
+                })
+                .bean(UserActivityDAO.class, "insertUserActivity(${userId}, ${contentId}, ${contentType}, ${actionType})")
+                .log("${body}")
+                .choice()
+                    .when(body().contains(true))
+                        .to("queue:sonyActivity")
+                    .otherwise()
+                        .to("stream:out");
+
+//                .setBody(simple("{\"title\":\"The Big Bang Theory\"}"));
 //                .toD("http4://localhost:13761/api/userActivities/${header.id}?bridgeEndpoint=true&urlRewrite=#urlRewrite");
         
     }
