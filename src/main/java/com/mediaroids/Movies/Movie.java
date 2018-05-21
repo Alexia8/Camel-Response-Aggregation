efcfaae81d6341663c04490920a1efa54ca1976e
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-05-21 15:13:32 +0200

    Create Movie class for JSON unmarshaling

---
+++ src/main/java/com/mediaroids/Movies/Movie.java
@@ -0,0 +1,19 @@
+package com.mediaroids.Movies;
+
+public class Movie {
+
+    public int id;
+    public String title;
+    public String description;
+    public String posterUrl;
+    public String fileUrl;
+
+    public void setTitle(String title) {this.title = title;}
+    public String getTitle() {return title;}
+
+    public void setDescription(String description) {this.description=description;}
+    public String getDescription() {return description;}
+
+
+
+}
